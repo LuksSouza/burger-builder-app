91563a7e27a1b6759e8e904ab2c44d27f2853ce6
Lucas Souza <lucas-machado@outlook.com>
2021-01-20 15:02:00 +0000

    Ajusted toolbar and added sidedrawer to be a mobile first app

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -3,11 +3,11 @@ import React from 'react';
 import classes from './NavigationItem.module.css';
 
 const navigationItem = (props) => (
-    <ul className={classes.NavigationItem}>
+    <li className={classes.NavigationItem}>
         <a 
             href={props.link}
             className={props.active ? classes.active : null}>{props.children}</a>
-    </ul>
+    </li>
 );
 
 export default navigationItem;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,12 +3,15 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import classes from './Toolbar.module.css'
 import NavigationItens from '../NavigationItems/NavigationItems';
+import DrawerToogle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo />
-        <nav>
+        <DrawerToogle clicked={props.drawerToogleClicked} />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
             <NavigationItens />
         </nav>
     </header>
